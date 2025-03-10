02d88ff63ef1e5c4d34336b3f3e6e234fc92b855
francktshibala <franck1tshibala@gmail.com>
2025-03-10 01:36:16 -0600

    Added to the project

--- server.js
+++ server.js
@@ -5,29 +5,31 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // not at views root
+
+
+/* ***********************
+ * Routes
+ *************************/
+app.use(static);
+
 // Index route
 app.get("/", function(req, res){
   res.render("index", { title: "Home" });
 });
 
 
-/* ***********************
- * View Engine and Templates
- *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
-
-
-/* ***********************
- * Routes
- *************************/
-app.use(static)
 
 /* ***********************
  * Local Server Information
@@ -40,5 +42,5 @@ const host = process.env.HOST
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
